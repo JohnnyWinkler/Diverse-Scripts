7ba163b21df4ffb3cae4ca06ea777fa911ecad43
Johnny Winkler <j.winkler@csm.int>
2021-02-23 14:02:21 +0100

    Update How to migrate on-premises Resource mailboxes.ps1

--- How to migrate on-premises Resource mailboxes.ps1
+++ How to migrate on-premises Resource mailboxes.ps1
@@ -1,6 +1,6 @@
 $UserCredential = Get-Credential
 
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "https://outlook.office365.com/powershell-liveid/ Jump " -Credential $UserCredential -Authentication Basic –AllowRedirection
+$SESSION = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $UserCredential -Authentication Basic –AllowRedirection
 Import-PSSession $Session
 
 $OnPremAdmin=Get-Credential

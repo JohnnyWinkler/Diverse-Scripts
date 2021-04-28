8e429a81b687356471139d82d04e3d86a8a8984e
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-04-28 14:22:41 +0200

    Update [AutoPilot] Get Windows AutoPilot information.ps1

--- [AutoPilot] Get Windows AutoPilot information.ps1
+++ [AutoPilot] Get Windows AutoPilot information.ps1
@@ -2,5 +2,5 @@
 Install-Script -Name Get-WindowsAutoPilotInfo
 
 # Nun wird ein Auszug mit den Nötigen Informationen gemacht. Dieser wird im Download-Ordner des aktuellen Users als .csv Datei abgespeichert.
-# Beim Dateinamen wird zusätzlich noch der Computername hinzugefügt.
+# Beim Dateinamen wird zusätzlich noch der Computername hinzugefügt. Wichtig: Muss als Administrator ausgeführt werden
 Get-WindowsAutoPilotInfo -outputfile C:\Users\${env:username}\Downloads\${env:computername}_AutoPilot.csv

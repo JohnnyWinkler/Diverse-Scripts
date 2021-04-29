d22b9db5ff51fe93f619d96fbc98c38cdbb6bdf4
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-04-29 16:43:46 +0200

    Create [Intune] Convert .exe File in .intunewin for Deployment via Intune.ps1

---
+++ [Intune] Convert .exe File in .intunewin for Deployment via Intune.ps1
@@ -0,0 +1,4 @@
+# Die .exe Datei des Tools ist zu Demonstrationszwecken unter C:\Microsoft-Win32-Content-Prep-Tool-master abgespeichert.
+
+cd C:\Microsoft-Win32-Content-Prep-Tool-master
+.\IntuneWinApp.Util.exe -c [SourceFolder] -s [SourceEXEFile] -o [DestinationFolder] -q

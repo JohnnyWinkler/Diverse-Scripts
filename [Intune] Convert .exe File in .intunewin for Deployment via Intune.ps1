13ce293b05f69fe6d0ccff39bc7c5ad079c9cf54
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-04-29 16:49:44 +0200

    Update [Intune] Convert .exe File in .intunewin for Deployment via Intune.ps1

--- [Intune] Convert .exe File in .intunewin for Deployment via Intune.ps1
+++ [Intune] Convert .exe File in .intunewin for Deployment via Intune.ps1
@@ -1,4 +1,4 @@
-# Die .exe Datei des Tools ist zu Demonstrationszwecken unter C:\Microsoft-Win32-Content-Prep-Tool-master abgespeichert.
+# Die .exe Datei des Tools ist zu Demonstrationszwecken unter C:\Microsoft-Win32-Content-Prep-Tool abgespeichert.
 # Das Repository kann hier von Github heruntergeladen werden: https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool
 
 cd C:\Microsoft-Win32-Content-Prep-Tool-master

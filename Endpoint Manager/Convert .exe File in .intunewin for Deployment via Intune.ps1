34e832d258077038c28603d554359a48a0aa19e7
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-06-03 09:55:14 +0200

    Changed "cd" to "Set-Location"

--- Endpoint Manager/Convert .exe File in .intunewin for Deployment via Intune.ps1
+++ Endpoint Manager/Convert .exe File in .intunewin for Deployment via Intune.ps1
@@ -1,7 +1,7 @@
 # Die .exe Datei des Tools ist zu Demonstrationszwecken unter C:\Microsoft-Win32-Content-Prep-Tool abgespeichert.
 # Das Repository kann hier von Github heruntergeladen werden: https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool
 
-cd C:\Microsoft-Win32-Content-Prep-Tool
+Set-Location C:\Microsoft-Win32-Content-Prep-Tool
 .\IntuneWinAppUtil.exe -c [SourceFolder] -s [SourceEXEFile] -o [DestinationFolder] -q
 
 # Beispiel für die konvertierung von 7-Zip in einem Ordner, welcher sich in meinem OneDrive befindet. Die konvertierte Datei wird wieder im gleichen Ordner abgespeichert.

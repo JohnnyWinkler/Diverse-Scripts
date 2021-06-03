2f06eac85da7246a7cbb5b7b908a2b23f5e84a91
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-06-03 15:45:07 +0200

    Reuploaded PowerShell Script as Executable

--- Endpoint Manager/Get Windows AutoPilot information.ps1
+++
@@ -1,6 +0,0 @@
-# Zu beginn muss das Script installiert werden
-Install-Script -Name Get-WindowsAutoPilotInfo -Verbose -Force
-
-# Nun wird ein Auszug mit den Nötigen Informationen gemacht. Dieser wird im Download-Ordner des aktuellen Users als .csv Datei abgespeichert.
-# Beim Dateinamen wird zusätzlich noch der Computername hinzugefügt. Wichtig: Muss als Administrator ausgeführt werden
-Get-WindowsAutoPilotInfo -outputfile C:\Users\${env:username}\Downloads\${env:computername}_AutoPilot.csv

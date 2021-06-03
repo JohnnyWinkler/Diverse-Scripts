be752973fd809ec5eb660e0a935c18cd8f7b97f8
Johnny Winkler <johnnyf.winkler@hotmail.com>
2021-06-03 16:49:41 +0200

    Reuploaded PowerShell with Executable

--- Endpoint Manager/Convert .exe File in .intunewin for Deployment via Intune.ps1
+++
@@ -1,8 +0,0 @@
-# Die .exe Datei des Tools ist zu Demonstrationszwecken unter C:\Microsoft-Win32-Content-Prep-Tool abgespeichert.
-# Das Repository kann hier von Github heruntergeladen werden: https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool
-
-Set-Location C:\Microsoft-Win32-Content-Prep-Tool
-.\IntuneWinAppUtil.exe -c [SourceFolder] -s [SourceEXEFile] -o [DestinationFolder] -q
-
-# Beispiel für die konvertierung von 7-Zip in einem Ordner, welcher sich in meinem OneDrive befindet. Die konvertierte Datei wird wieder im gleichen Ordner abgespeichert.
-.\IntuneWinAppUtil.exe -c "C:\temp" -s 7z1900.exe -o "C:\temp" -q
